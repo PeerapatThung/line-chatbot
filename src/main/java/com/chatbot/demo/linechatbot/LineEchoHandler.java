05cb34e786bf66d1bc8e6037c8002bd6d9f17577
unknown <peerapat.next@gmail.com>
2022-03-18 16:37:54 +0700

    update

--- src/main/java/com/chatbot/demo/linechatbot/LineEchoHandler.java
+++ src/main/java/com/chatbot/demo/linechatbot/LineEchoHandler.java
@@ -8,14 +8,28 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @LineMessageHandler
 @Slf4j
 public class LineEchoHandler {
+    int state = 1;
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
+//        System.out.println("event: " + event);
+//        return new TextMessage(event.getMessage().getText());
+        if(state == 1){
+            state = 2;
+            redisTemplate.opsForHash().put("LineCache", event.getSource().getUserId(),state);
+            return TextMessage.builder().text(event.getMessage().getText()).build();
+        }else{
+            state = 1;
+            redisTemplate.opsForHash().put("LineCache", event.getSource().getUserId(),state);
+            return TextMessage.builder().text("xx").build();
+        }
     }
 
     @EventMapping
